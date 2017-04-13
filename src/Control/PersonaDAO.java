13f2938a72d182c7c98bd13c325a476b711c3b40
jose luis Rodriguez <jose luis Rodriguez@joseluisRodrigu>
2017-04-13 10:59:25 -0500

    cambios del desarrollo

--- src/Control/PersonaDAO.java
+++ src/Control/PersonaDAO.java
@@ -4,15 +4,20 @@ package Control;
 import Modelo.Persona;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author jose luis Rodriguez
  */
 public class PersonaDAO {
-      SentenciaSQL sentencia = new SentenciaSQL();
+    
+     SentenciaSQL sentencia = new SentenciaSQL();
      String query;
-     Integer conteo = 0;
+     String queryCodigo ="";
+     ResultSet resultset;
+     
     public void registrarPersona(Persona persona)throws SQLException{
         persona.setNombres("juan");
         persona.setApellidos("cuadrado");
@@ -23,8 +28,9 @@ public class PersonaDAO {
         persona.setTipoDocumento("CC");
         persona.setNumeroDocumento("155555");
         
+        queryCodigo = "select count(cod_persona) conteo from persona";
         
-        ResultSet result = sentencia.gestionarConsulta("select count(cod_persona) conteo from persona");
+        ResultSet result = sentencia.gestionarConsulta(queryCodigo);
       
         persona.setCodPersona(sentencia.contarCodigos(result));
         
@@ -38,6 +44,116 @@ public class PersonaDAO {
     sentencia.gestionarRegistro(query);
     }
     
+    public void modificarPersona(Persona persona){
+        
+                persona.setNombres("juangui");
+        persona.setApellidos("cuadradosqui");
+        persona.setDireccion("calle 5aa");
+        persona.setTelefono("3555545");
+        persona.setCelular("3154555545");
+        persona.setEmail("jc@gmail.com");
+        persona.setTipoDocumento("CC");
+        persona.setNumeroDocumento("155555");
+    
+    query =    "UPDATE persona" +
+        "	SET  nombres='"+persona.getNombres()+"', apellidos='"+persona.getApellidos()+"',"
+            + " direccion='"+persona.getDireccion()+"', telefono='"+persona.getTelefono()+"',"
+            + " celular='"+persona.getCelular()+"', correo='"+persona.getEmail()+"',"
+            + " tipo_documento='"+persona.getTipoDocumento()+"',"
+            + " numero_documento = '"+persona.getNumeroDocumento()+"' " +
+        "	WHERE numero_documento = '"+persona.getNumeroDocumento()+"'";
+    
+    sentencia.gestionarRegistro(query);
+    
+    }
+    
+    public void eliminarPersona(String numeroDocumento){
+            
+            query = "DELETE FROM persona " +
+                    "WHERE numero_documento = '"+numeroDocumento+"'";
+            sentencia.gestionarRegistro(query);
+    }
+    
+    public List <Persona> consultarPersona()throws SQLException{
+        
+        
+        
+            query = "SELECT cod_persona, nombres, apellidos, direccion, telefono,"
+            + " celular, correo, tipo_documento, numero_documento " +
+              " FROM persona ORDER BY cod_persona ASC";
+            
+            resultset = sentencia.gestionarConsulta(query);
+            
+          
+         return mapearPersonas(resultset);
+    }
+    
+        public Persona consultarPersonaNumeroDocumento(String numeroDocumento){
+            query = "SELECT cod_persona, nombres, apellidos, direccion, telefono,"
+            + " celular, correo, tipo_documento, numero_documento " +
+              " FROM persona WHERE numero_documento = '"+numeroDocumento+"'"
+                    + "  ORDER BY cod_persona ASC";
+            
+            resultset = sentencia.gestionarConsulta(query);
+            
+            return  mapearPersona(resultset);
+    }
+        
+        public List <Persona> mapearPersonas(ResultSet resultset){
+            
+                List <Persona> listadoPersonas = new ArrayList<>();
+                Persona persona = new Persona();
+                
+            try {
+                
+              while(resultset.next()){
+                
+                persona.setCodPersona(resultset.getInt("cod_persona"));
+                persona.setNombres(resultset.getString("nombres"));
+                persona.setApellidos(resultset.getString("apellidos"));
+                persona.setDireccion(resultset.getString("direccion"));
+                persona.setTelefono(resultset.getString("telefono"));
+                persona.setCelular(resultset.getString("celular"));
+                persona.setEmail(resultset.getString("correo"));
+                persona.setTipoDocumento(resultset.getString("tipo_documento"));
+                persona.setNumeroDocumento(resultset.getString("numero_documento"));
+                
+                listadoPersonas.add(persona);
+                System.out.println(" persona "+persona.getNombres());
+                
+            }
+            } catch (Exception e) {
+            }
+      
+        return listadoPersonas;
+        
+        }
+    
+                public Persona mapearPersona(ResultSet resultset){
+                
+                Persona persona = new Persona();
+            try {
+                    
+              if(resultset.next()){
+                
+                persona.setCodPersona(resultset.getInt("cod_persona"));
+                persona.setNombres(resultset.getString("nombres"));
+                persona.setApellidos(resultset.getString("apellidos"));
+                persona.setDireccion(resultset.getString("direccion"));
+                persona.setTelefono(resultset.getString("telefono"));
+                persona.setCelular(resultset.getString("celular"));
+                persona.setEmail(resultset.getString("correo"));
+                persona.setTipoDocumento(resultset.getString("tipo_documento"));
+                persona.setNumeroDocumento(resultset.getString("numero_documento"));
+                                
+                System.out.println(" persona "+persona.getNombres());
+                
+            }
+            } catch (Exception e) {
+            }
+      
+        return persona;
+        }
     
     
     
@@ -45,7 +161,11 @@ public class PersonaDAO {
         PersonaDAO p = new PersonaDAO();
         Persona persona = new Persona();
         
-        p.registrarPersona(persona);
+       // p.registrarPersona(persona);
+        //p.modificarPersona(persona);
+        //p.eliminarPersona("155555");
+        //p.consultarPersona();
+        p.consultarPersonaNumeroDocumento("12345");
     }
     
 }
---
+++ src/Control/ProductoDAO.java
@@ -0,0 +1,192 @@
+
+package Control;
+
+
+import Modelo.Producto;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+
+/**
+ *
+ * @author jose luis Rodriguez
+ */
+public class ProductoDAO {
+    
+     SentenciaSQL sentencia = new SentenciaSQL();
+     String query;
+     String queryCodigo ="";
+     ResultSet resultset;
+     
+    public void registrarProducto(Producto producto)throws SQLException, ParseException{
+
+     
+        producto.setNombreProdcto("Bandas");
+        producto.setDescripcion("compatibles con eco deluxe y splendor");
+        producto.setTipoProducto("fre");
+        producto.setCantidad(15);
+        producto.setFabricante("Japan");
+        producto.setFechaIngreso(new SimpleDateFormat("dd-MM-yyyy").parse("05-05-2017"));
+        producto.setValor(10000);
+        producto.setEstado("Activo");
+        
+        queryCodigo = "select count(cod_producto) conteo from producto";
+        
+        ResultSet result = sentencia.gestionarConsulta(queryCodigo);
+      
+        producto.setCodProducto(sentencia.contarCodigos(result));
+        
+        query = "INSERT INTO producto( " +
+                  " cod_producto, nombre_producto, descripcion,"
+                + " tipo_producto, cantidad, fabricante,"
+                + " fecha_ingreso, valor,"
+                + " estado) " 
+                +" VALUES ("+producto.getCodProducto()+", '"+producto.getNombreProdcto()+"',"
+                + " '"+producto.getDescripcion()+"', '"+producto.getTipoProducto()+"',"
+                + " "+producto.getCantidad()+", '"+producto.getFabricante()+"',"
+                + " '"+producto.getFechaIngreso()+"', "+producto.getValor()+","
+                + " '"+producto.getEstado()+"')";
+        
+        sentencia.gestionarRegistro(query);
+    }
+    
+    public void modificarProducto(Producto producto){
+        
+        producto.setNombreProdcto("pastillas");
+        producto.setDescripcion("compatibles con wave y splendor");
+        producto.setTipoProducto("fre");
+        producto.setCantidad(5);
+        producto.setFabricante("honda");
+        producto.setFechaIngreso(Date.valueOf("01-05-2015"));
+        producto.setValor(10000);
+        producto.setEstado("Activo");
+        producto.setCodProducto(1);
+    
+    query =    "UPDATE producto " 
+            + " SET  nombre_producto='"+producto.getNombreProdcto()+"',"
+            + " descripcion='"+producto.getDescripcion()+"',"
+            + " tipo_producto='"+producto.getTipoProducto()+"',"
+            + " cantidad="+producto.getCantidad()+","
+            + " fabricante='"+producto.getFabricante()+"',"
+            + " fecha_ingreso='"+producto.getFechaIngreso()+"',"
+            + " valor="+producto.getValor()+","
+            + " estado='"+producto.getEstado()+"'" 
+            + " WHERE cod_producto = "+producto.getCodProducto()+"";
+    
+    sentencia.gestionarRegistro(query);
+    
+    }
+    
+    public void eliminarProducto(String codProducto){
+            
+            query = "DELETE FROM producto " +
+                    "WHERE cod_producto = "+codProducto+"";
+            sentencia.gestionarRegistro(query);
+    }
+    
+    public List <Producto> consultarProducto()throws SQLException{
+        
+        
+        
+            query = "SELECT cod_producto, nombre_producto, descripcion,"
+                    + " tipo_producto, cantidad, fabricante, fecha_ingreso,"
+                    + " valor, estado " +
+                      " FROM producto ORDER BY cod_producto ASC";
+            
+            resultset = sentencia.gestionarConsulta(query);
+            
+          
+         return mapearProductos(resultset);
+    }
+    
+        public Producto consultarProductoCodProducto(Integer codProducto){
+            
+            query = "SELECT cod_producto, nombre_producto, descripcion,"
+                    + " tipo_producto, cantidad, fabricante, fecha_ingreso,"
+                    + " valor, estado " +
+                      " FROM producto WHERE cod_producto = "+codProducto+" ORDER BY cod_producto ASC";
+            
+            resultset = sentencia.gestionarConsulta(query);
+            
+            return  mapearProducto(resultset);
+    }
+        
+        public List <Producto> mapearProductos(ResultSet resultset){
+            
+                List <Producto> listadoProductos = new ArrayList<>();
+                Producto producto = new Producto();
+                
+            try {
+                
+              while(resultset.next()){
+                
+                producto.setCodProducto(resultset.getInt("cod_producto"));
+                producto.setNombreProdcto(resultset.getString("nombre_producto"));
+                producto.setDescripcion(resultset.getString("descripcion"));
+                producto.setTipoProducto(resultset.getString("tipo_producto"));
+                producto.setCantidad(resultset.getInt("cantidad"));
+                producto.setFabricante(resultset.getString("fabricante"));
+                producto.setFechaIngreso(resultset.getDate("fecha_ingreso"));
+                producto.setValor(resultset.getInt("valor"));
+                producto.setEstado(resultset.getString("estado"));                
+                listadoProductos.add(producto);
+
+                System.out.println(" Producto "+producto.getNombreProdcto());
+                System.out.println(" Producto "+producto.getFechaIngreso());
+                
+            }
+            } catch (Exception e) {
+            }
+      
+        return listadoProductos;
+        
+        }
+    
+                public Producto mapearProducto(ResultSet resultset){
+                
+                Producto producto = new Producto();
+            try {
+                    
+              if(resultset.next()){
+                
+                producto.setCodProducto(resultset.getInt("cod_producto"));
+                producto.setNombreProdcto(resultset.getString("nombre_producto"));
+                producto.setDescripcion(resultset.getString("descripcion"));
+                producto.setTipoProducto(resultset.getString("tipo_producto"));
+                producto.setCantidad(resultset.getInt("cantidad"));
+                producto.setFabricante(resultset.getString("fabricante"));
+                producto.setFechaIngreso(resultset.getDate("fecha_ingreso"));
+                producto.setValor(resultset.getInt("valor"));
+                producto.setEstado(resultset.getString("estado"));                
+                                               
+                System.out.println(" producto "+producto.getNombreProdcto());
+                System.out.println(" Producto "+producto.getFechaIngreso());
+                
+            }
+            } catch (Exception e) {
+            }
+      
+        return producto;
+        }
+    
+    
+    
+    public static void main(String[] args) throws SQLException, ParseException {
+        ProductoDAO p = new ProductoDAO();
+        Producto producto = new Producto();
+        
+       // p.registrarProducto(producto);
+       //p.consultarProducto();
+       p.consultarProductoCodProducto(1);
+        
+
+    }
+
+    
+}
