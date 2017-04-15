2f6c2777f7ccb6ab2757eea191a8528866fa6257
jose luis Rodriguez <jose luis Rodriguez@joseluisRodrigu>
2017-04-15 17:59:18 -0500

    Creacion de clases DAO

---
+++ src/Modelo/TipoUsuario.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author jose luis Rodriguez
+ */
+public class TipoUsuario implements CatalogoI{
+    
+    private String llave;
+    
+    private String valor;
+
+    @Override
+    public String getLlave() {
+        return llave;
+    }
+
+    @Override
+    public String getValor() {
+        return valor;
+    }
+
+    @Override
+    public void setLlave(String llave) {
+        this.llave = llave;
+    }
+
+    @Override
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+   
+}
