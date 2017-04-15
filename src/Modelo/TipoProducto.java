93f0a7347117a6fd00e03f587946eb6657b4f28f
jose luis Rodriguez <jose luis Rodriguez@joseluisRodrigu>
2017-04-15 18:03:23 -0500

    Creacion clases DAO

--- src/Modelo/TipoProducto.java
+++ src/Modelo/TipoProducto.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
  *
  * @author jose luis Rodriguez
  */
-public class tipoUsuario implements CatalogoI{
+public class TipoProducto implements CatalogoI{
     
     private String llave;
     
@@ -36,4 +32,6 @@ public class tipoUsuario implements CatalogoI{
     }
 
    
+
+    
 }
